ffa9fd65d23985c9bb7dfe15ec80d8ca3bf26e82
Chris Gardner <ctgardner89@gmail.com>
2016-07-23 17:00:36 -0400

    finished my homewrk

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ var app = express()
 app.set("view engine", "hbs")
 app.use(express.static("public"))
 var compliments = [
-  "Your mother love you",
+  "Your mother loves you",
   "A Lanister always pays his debts",
   "Winter is coming",
   "Only a Sith deals in absolutes",
-  "It's a brand new day, brand new you!",
+  "It's a brand new day, brand new you",
   "The Force is strong with you"
 ]
 
-var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+var colors = ["#EC192D", "#0080FF","#01DF3A","#722CA5"]
 
 Array.prototype.random = function(){
   return this[Math.floor(this.length * Math.random())]
 }
 
-app.get("/", function(req, res){
+app.get("/:name?", function(req, res){
   res.render("index", {
     compliment: compliments.random(),
-    color: colors.random()
+    color: colors.random(),
+    name: req.params.name
   })
 })
 
